30b3a528833401c1e964a6cfb2f4a8ffd74b5c94
ayahdweikat <dweikatayah99@gmail.com>
2023-02-23 16:04:51 +0200

    version-sixteen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
+// import { createTheme, ThemeProvider , colors } from "@mui/material";
+// // ThemeProvider
+// const theme =  createTheme({
+//   palette:{
+//     // color:colors.grey[100]
+//     secondary:{
+//       main:'#DFE0EB',
+//     },
+//   },
+// })
 function App() {
   return (
     <div className="App">
---
+++ src/Components/Header/HeaderTheme.jsx
@@ -0,0 +1,32 @@
+// const theme = createTheme({
+//     typography: {
+//       fontFamily: [
+//         'Mulish', 'sans-serif'
+//       ].join(','),
+//     } , {
+//         lineHeight: 30.12
+//     },
+//     {fontWeight: 700},
+//   });
+
+// sidebar // #DDE2FF // #494950 // #A4A6B3 // #363740 
+// header // #252733 // #DFE0EB 
+// userview // #2050AD // #fff // #252733 // #c5c7cd 
+// users // #0000ff // #9fa2b4 // #c5c7cd // #252733 // #dfe0eb // #fff
+
+
+
+
+// .ggg{
+//     color:#fff;
+//     color:#c5c7cd;
+//     color:#dfe0eb;    
+//     color:#9fa2b4;
+//     color:#DDE2FF;
+//     color:#A4A6B3;
+//     color:#494950;
+//     color:#363740;
+//     color:#252733;
+//     color:#2050AD;
+//     color:#0000ff;
+// }
